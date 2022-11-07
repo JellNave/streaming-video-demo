6b9a83ebc25138645a80043e084b94fa6ceffa4a
zhang.guangyu <1471468979@qq.com>
2022-11-07 22:40:07 +0800

    feat:* n ezplayer

--- src/pages/easyplayer.vue
+++ src/pages/easyplayer.vue
@@ -1,9 +1,43 @@
 <template>
-    <h2 class="tit"> EASY PLAYER Vue3不支持 </h2>
+    <h2 class="tit"> EASY PLAYER </h2>
+    <p>vue3使用存在问题</p>
     <a
         href="https://github.com/yhb980709/vue-easyplayer-demo"
         target="_blank"
         rel="noopener noreferrer"
         >vue-easyplayer-demo</a
     >
+    <div ref="videoRef" class="video-player"> </div>
 </template>
+
+<script lang="ts" setup>
+    // import EasyPlayer from '@easydarwin/easyplayer';
+    import { onMounted, nextTick, ref } from 'vue';
+    const videoRef = ref();
+    const videSrc =
+        'http://219.151.31.38/liveplay-kk.rtxapp.com/live/program/live/hnwshd/4000000/mnf.m3u8';
+    onMounted(() => {
+        nextTick(() => {
+            videoRef.value.innerHTML = `<easy-player
+            id="easy-player"
+            live="false"
+      stretch="true"
+        ></easy-player>`;
+            nextTick(() => {
+                const player = document.querySelector('#easy-player');
+                (player as HTMLElement).setAttribute('video-url', videSrc);
+            });
+        });
+    });
+</script>
+
+<style>
+    .video-player {
+        width: 500px;
+        height: 500px;
+    }
+    /* #easy-player {
+        width: 100%;
+        height: 100%;
+    } */
+</style>
